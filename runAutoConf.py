f669906fddb54650dc0371e87193bb1e9fb18539
RSF Data Group <rsfdata@wind.eol.ucar.edu>
2021-02-27 18:10:28 +0000

    Updating runAutoConf.py to python3. Rerunning runAutoConf.py

--- runAutoConf.py
+++ runAutoConf.py
@@ -6,6 +6,7 @@
 #
 #===========================================================================
 
+from __future__ import print_function
 import os
 import sys
 import subprocess
@@ -45,8 +46,8 @@ def main():
         options.debug = True
     
     if (options.debug == True):
-        print >>sys.stderr, "Running %s:" % thisScriptName
-        print >>sys.stderr, "  Dir:", options.dir
+        print("Running %s:" % thisScriptName, file=sys.stderr)
+        print("  Dir:", options.dir, file=sys.stderr)
 
     # go to the dir
 
@@ -96,22 +97,22 @@ def runAutoConf():
 def runCommand(cmd):
 
     if (options.debug == True):
-        print >>sys.stderr, "running cmd:", cmd, " ....."
+        print("running cmd:", cmd, " .....", file=sys.stderr)
     
     try:
         retcode = subprocess.check_call(cmd, shell=True)
         if retcode != 0:
-            print >>sys.stderr, "Child exited with code: ", retcode
+            print("Child exited with code: ", retcode, file=sys.stderr)
             sys.exit(1)
         else:
             if (options.verbose == True):
-                print >>sys.stderr, "Child returned code: ", retcode
-    except OSError, e:
-        print >>sys.stderr, "Execution failed:", e
+                print("Child returned code: ", retcode, file=sys.stderr)
+    except OSError as e:
+        print("Execution failed:", e, file=sys.stderr)
         sys.exit(1)
 
     if (options.debug == True):
-        print >>sys.stderr, ".... done"
+        print(".... done", file=sys.stderr)
     
 ########################################################################
 # Run - entry point
